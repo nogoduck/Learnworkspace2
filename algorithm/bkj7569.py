30e8fc5706f6cce465c705f9f1d85239a25f1285
adu <nogo22@naver.com>
2021-08-25 13:49:24 +0900

    bkj7569 - 진행중2

--- algorithm/bkj7569.py
+++ algorithm/bkj7569.py
@@ -8,21 +8,25 @@ def printMap(Map):
     for i in Map:
         print(*i, " ")
 
-# 맵에 0이 존재하면 True값 반환
+# 방문을 체크하는 배열 초기화
+def setCheckArr():
+    checkArr = [[0 for _ in range(N)] for _ in range(N * H)]
+
+# 배열에 0이 존재하면 True값 반환
 def checkNull():
     for i in range(0, len(arr)):
         for j in range(0, len(arr[i])):
             if arr[i][j] == 0:
                 return True
 
-
+# 배열안에 1의 좌표값을 탐색 후 리스트에 삽입 
 def enableIndex():
     for i in range(0, len(arr)):
         for j in range(0, len(arr[i])):
             if arr[i][j] == 1:
                 enableList.append([i, j])
          
-
+# BFS 탐색
 def bfs(a, b):
     q = deque()
     q.append([a, b])
@@ -37,25 +41,28 @@ def bfs(a, b):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0 <= ny < N and checkArr[nx][ny] == 0:
-                
                 checkArr[nx][ny] = 1
                 q.append([nx, ny])
 
-
-# 0, -1, 1 
 M, N, H =  map(int, input().split())
-
 arr = [list(map(int, input().split())) for _ in range(N * H)]
 checkArr = [[0 for _ in range(N)] for _ in range(N * H)]
 
-
-# 최초 입력시 배열에 0이 존재하지 않으면 아래 로직을 수행하지 않고 바로 종료
+# 최초 입력시 배열에 0이 존재하면 로직을 수행하고
+# 존재하지 않을 시 0을 출력하고 종료한다.
 if checkNull():
-    
-    else:
+    # 토마토가 존재하는 좌표 삽입
+    enableList = []
+    enableIndex()
+    print('enableList >> ', enableList)
 
-        enableList = []
-        enableIndex()
-        print('enableList >> ', enableList)
+    if enableList:
+        for i in range(0, len(enableList)):
 
-        printMap(arr)
+            print(i)
+            # bfs(enableList[i])
+            
+
+
+else:
+    print(0)
