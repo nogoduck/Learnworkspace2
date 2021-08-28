894ffe76182f587c6541a887ec7c465e373f7b1e
adx <nogo22@naver.com>
2021-08-29 08:37:34 +0900

    p26 - 해결

---
+++ algorithm/practice/p26.py
@@ -0,0 +1,33 @@
+# 자릿수의 합
+import sys
+sys.stdin=open("input.txt", "r")
+
+def digit_sum(n):
+    sum = 0
+
+    # case 2
+    for v in str(n):
+        sum += int(v)
+
+    # case 1
+    # while n > 0:
+    #     sum += n % 10
+    #     n //= 10
+    #     print(sum, n)
+
+    return sum
+
+
+n = int(input())
+arr = list(map(int , input().split()))
+max = -2147483648
+# res = 0
+for v in arr:
+    t = digit_sum(v)
+    if max < t:
+        max = t
+        res = v
+
+print(res)
+
+print(2 ** 31)
