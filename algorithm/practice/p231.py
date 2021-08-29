22b6a7022e02b37ae3d01e17386c42714965eaa5
adx <nogo22@naver.com>
2021-08-29 19:29:34 +0900

    p31 - 해결

---
+++ algorithm/practice/p231.py
@@ -0,0 +1,30 @@
+# 점수 계산
+
+import sys
+sys.stdin=open("input.txt", "r")
+
+n = int(input())
+
+
+# Case 2
+for i in range(n):
+    s = input().lower()
+    if s == s[::-1]:
+        print("#%d YES" %(i + 1))
+    else:
+        print("#%d NO" %(i + 1))
+        
+        
+    
+
+# Case 1
+# for i in range(n):
+#     s = input().lower()
+
+#     for j in range(len(s) // 2):
+#         if s[j] != s[-1 -j]:
+#             print("#%d NO" %(i + 1))
+#             break
+#     else:
+#         print("#%d YES" %(i + 1))
+                    
