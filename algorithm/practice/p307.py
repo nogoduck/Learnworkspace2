2302494edc5b5982e8f1fb7161bfb4880d1a344a
adx <nogo22@naver.com>
2021-09-04 19:42:52 +0900

    p308 - 해결

--- algorithm/practice/p307.py
+++ algorithm/practice/p307.py
@@ -1,6 +1,6 @@
 # 사과나무 (다이아몬드)
 import sys
-sys.stdin=open("input.txt", "r")
+# sys.stdin=open("input.txt", "r")
 
 def printArr(arr):
     for i in arr:
@@ -10,4 +10,21 @@ def printArr(arr):
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-printArr(arr)
+# printArr(arr)
+
+lt = n // 2
+rt = n // 2 + 1
+res = 0
+for i in range(n):
+    # print(lt, rt)
+    for j in range(lt, rt):
+        res += arr[i][j]
+    
+    if i < n // 2:
+        lt -= 1
+        rt += 1
+    else:
+        lt += 1
+        rt -= 1
+
+print(res)
