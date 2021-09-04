382a311565301dae8880869a8d6bac5ce7edc454
adx <nogo22@naver.com>
2021-09-04 17:54:11 +0900

    310 - 해결

--- algorithm/practice/p310.py
+++ algorithm/practice/p310.py
@@ -2,10 +2,32 @@
 import sys
 # sys.stdin=open("input.txt", "r")
 
+dx = [0, 0, 1, 1, 1, 2, 2, 2]
+dy = [1, 2, 0, 1, 2, 0, 1, 2]
+arr = [list(map(int, input().split())) for _ in range(9)]
+
 def printArr(arr):
     for i in arr:
         print(*i)
 
+def checkArr(nx, ny):
+    # print('nx, ny >> ', nx, ny)
+    global arr, cnt
+    check = [True] * 10
+    for i in range(8):
+        x = nx + dx[i]
+        y = ny + dy[i]
+        # print('x, y >> ', x, y)
+        num = arr[x][y]
+        if check[num]:
+            check[num] = False
+            continue
+        return 
+    cnt -= 1
 
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
+cnt = 9
+for i in range(0, 7, 3):
+    for j in range(0, 7, 3):
+        checkArr(i, j)
+            
+print("NO" if cnt else "YES")
