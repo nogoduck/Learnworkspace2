3b71ec9701746a5df06b7e3c48a8aea7bb53790d
adx <nogo22@naver.com>
2021-12-31 20:39:24 +0900

    이미지 업로드

--- webSocketExample/app.js
+++ webSocketExample/app.js
@@ -24,6 +24,7 @@ nunjucks.configure("views", {
 connect();
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
