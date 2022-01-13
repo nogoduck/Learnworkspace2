462f5a2158e3ba231870882edff4b57765974e79
adx <nogo22@naver.com>
2022-01-13 18:58:17 +0900

    express middleware

--- typescriptSetup/dist/app.js
+++ typescriptSetup/dist/app.js
@@ -1,10 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log("this is som middleware");
+    next();
+});
 app.get("/", function (req, res) {
-    res.send({ name: "LEeY", age: 99, frends: "dfe" });
+    res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:".concat(port));
--- typescriptSetup/src/app.ts
+++ typescriptSetup/src/app.ts
@@ -1,12 +1,49 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 const port: number = 8000;
 
+//app.use를 사용하면 미들웨어를 전체적으로 관리 (순서 중요)
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    next();
+  }
+);
+
+//특정 라우터만 미들웨어를 추가하기 위해선 app.get과 next 파라미터를 추가해준다.
+app.get(
+  "/cats/som",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log("this is som middleware");
+    next();
+  }
+);
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ name: "LEeY", age: 99, frends: "dfe" });
+  res.send({ cats: Cat });
 });
 
+app.get(
+  "/cats/blue",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.send({ blue: Cat[0] });
+  }
+);
+
+app.get("/cats/som", (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
+});
+
+//일치하는 경로가 없을때 처리할 라우터
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
+    // next();
+  }
+);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
