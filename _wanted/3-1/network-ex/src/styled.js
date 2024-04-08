f0126ef847a2c6c06afb00972acd8150b42d2f56
adx <nogo22@naver.com>
2024-04-08 23:47:38 +0900

    230408 수업

---
+++ _wanted/3-1/network-ex/src/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  input {
+    padding: 6px 8px;
+    margin-bottom: 8px;
+  }
+`;
